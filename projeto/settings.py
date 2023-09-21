dab043a582f24b800c7a7f4d0d3fc974f28baefd
Matheus Morais <morais629@gmail.com>
2023-09-21 11:57:36 -0300

    Finalizando seção 6

--- projeto/settings.py
+++ projeto/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'recipes/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
