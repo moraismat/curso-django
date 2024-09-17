badc660800158dbe828f7fa044cd2bf27285d834
matheus <matheus.morais@codata.pb.gov.br>
2024-09-17 10:51:11 -0300

    Aula sobre urls e views

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,21 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Home')
-
-def contato(request):
-    return HttpResponse('Contato')
-
-def sobre(request):
-    return HttpResponse('Sobre')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', contato),
-    path('contato/', sobre)
+    path('', include('recipes.urls'))
 ]
