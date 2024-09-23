f9fb383d2347277b6fa898ee917d9ca3709ba4cc
matheus <matheus.morais@codata.pb.gov.br>
2024-09-23 11:43:25 -0300

    Aula 46 - Injeção de Dados

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from utils.recipes.factory import make_recipe
 
+# Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Matheus Morais',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Matheus Morais',
+        'recipe': make_recipe(),
     })
