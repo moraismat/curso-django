fe6d94e2f1be02d25404a6afa8c1930b9312b9f3
matheus <matheus.morais@codata.pb.gov.br>
2024-09-24 11:10:36 -0300

    Aula 59 - Carregando os dados salvos

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', home, name="recipes-home"),
+    path('', home, name="recipes-home"),   
+    path('recipes/category/<int:category_id>/', category, name="recipes-category"),
     path('recipes/<int:id>/', recipe, name="recipes-recipe"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        is_published=True,
+        category__id=category_id
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
     })
 
 
